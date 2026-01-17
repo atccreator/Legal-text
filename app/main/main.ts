ad878f2d1725fdbde65d4f6e6f7ee4d5fba10e70
atccreator <chourasiyaji11@gmail.com>
2026-01-17 11:56:13 +0530

    basic setup

---
+++ app/main/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+//   win.loadURL('https://example.com')
+    win.loadFile(path.join(__dirname, '../renderer/index.html'))
+}
+
+app.whenReady().then(createWindow)
---
+++ app/renderer/src/App.tsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return <div>
+    <h1>Hello, Electron with React and TypeScript!</h1>
+    <h2>Created By Ankit Chourasiya</h2>
+  </div>
+}
